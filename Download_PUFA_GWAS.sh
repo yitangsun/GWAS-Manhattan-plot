b3ce70b4a994ef5f686a2884958be67da352c606
yitangsun <53280136+yitangsun@users.noreply.github.com>
2021-05-19 20:22:47 -0400

    Create Download_PUFA_GWAS.sh

---
+++ Download_PUFA_GWAS.sh
@@ -0,0 +1,14 @@
+cd /scratch/ys98038/UKB/plink2_format/COVID_19/Analyses/Genotype/
+
+#### May 19 2021
+wget https://gwas.mrcieu.ac.uk/files/met-d-Omega_3/met-d-Omega_3.vcf.gz
+
+wget https://gwas.mrcieu.ac.uk/files/met-d-Omega_6/met-d-Omega_6.vcf.gz
+
+wget https://gwas.mrcieu.ac.uk/files/met-d-PUFA/met-d-PUFA.vcf.gz
+
+wget https://gwas.mrcieu.ac.uk/files/met-d-LA/met-d-LA.vcf.gz
+
+wget https://gwas.mrcieu.ac.uk/files/met-d-DHA/met-d-DHA.vcf.gz
+
+
